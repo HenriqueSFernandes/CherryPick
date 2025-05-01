d34c9806d30c1b3107ec718b5da425489ab3008d
Process-ing <rbruno.p.o@gmail.com>
2025-05-02 00:10:01 +0100

    Add makasar fonts

--- cherrypick-fe/src/components/header/logo.tsx
+++ cherrypick-fe/src/components/header/logo.tsx
@@ -1,6 +1,8 @@
+import { makasar } from "@/fonts";
+
 export default async function Logo() {
   return (
-    <div className="flex items-center">
+    <div className={`flex items-center ${makasar.className}`}>
       <span className="text-2xl font-bold text-gray-900">Cherry Pick</span>
     </div>
   );
