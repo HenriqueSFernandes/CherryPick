520f589705ddff65e6dfafe82264abd0a9179686
Process-ing <rbruno.p.o@gmail.com>
2025-05-02 00:40:23 +0100

    Add login button

--- cherrypick-fe/src/components/header/header.tsx
+++ cherrypick-fe/src/components/header/header.tsx
@@ -1,9 +1,12 @@
+import { Button } from "../ui/button";
 import Logo from "./logo";
 
 export default async function Header() {
   return (
-    <header className="h-16 px-4 flex items-center">
+    <header className="h-16 px-4 grid grid-cols-[auto_1fr_auto] items-center">
       <Logo />
+      <span></span>
+      <Button className="font-bold bg-dark-red">Login</Button>
     </header>
   );
 }
