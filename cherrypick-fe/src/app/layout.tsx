ea5b8fae6b0356f466aab8886cfab4570f56b200
Process-ing <rbruno.p.o@gmail.com>
2025-05-01 23:50:13 +0100

    Create header

--- cherrypick-fe/src/app/layout.tsx
+++ cherrypick-fe/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
+import Header from "@/components/header/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +32,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Header />
+        <main>{children}</main>
         <SpeedInsights />
       </body>
     </html>
