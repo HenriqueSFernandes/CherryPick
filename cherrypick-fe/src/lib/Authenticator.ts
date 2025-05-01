30954adb85971995354045d614d2f3d89647b65e
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2025-05-01 19:34:13 +0100

    feat: base login prototype

---
+++ cherrypick-fe/src/lib/Authenticator.ts
@@ -0,0 +1,36 @@
+import client from "@/lib/AppwriteClient";
+
+import { Account, ID } from "appwrite";
+
+export const register = async (
+	email: string,
+	password: string,
+	passwordConfirmation: string,
+) => {
+	if (password !== passwordConfirmation) {
+		throw new Error("Passwords do not match");
+	}
+	const account = new Account(client);
+	const promise = account.create(ID.unique(), email, password);
+	promise.then(
+		function(response) {
+			console.log(response);
+		},
+		function(error) {
+			console.log(error);
+		},
+	);
+};
+
+export const login = async (email: string, password: string) => {
+	const account = new Account(client);
+	const promise = account.createEmailPasswordSession(email, password);
+	promise.then(
+		function(response) {
+			console.log(response);
+		},
+		function(error) {
+			console.log(error);
+		},
+	);
+};
