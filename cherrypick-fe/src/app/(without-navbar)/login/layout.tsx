7dd329944ff6e9ecb7ee792ced8338346cff74de
Clara Sousa <clarasousa2004@hotmail.com>
2025-05-02 19:54:31 +0100

    login prettier, lacks footer improvements

--- cherrypick-fe/src/app/(without-navbar)/login/layout.tsx
+++ cherrypick-fe/src/app/(without-navbar)/login/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import "./../globals.css";
+import "../../globals.css";
 import Footer from "@/components/footer/footer";
 import Gradients from "@/components/general/gradients";
 import { AuthProvider } from "@/context/AuthContext";
