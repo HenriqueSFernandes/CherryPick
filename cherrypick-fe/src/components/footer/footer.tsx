0ceefcbbdfd15bc4e233a3df3c36367eabcfaa68
Process-ing <rbruno.p.o@gmail.com>
2025-05-02 10:25:15 +0100

    Refactor logo structure

--- cherrypick-fe/src/components/footer/footer.tsx
+++ cherrypick-fe/src/components/footer/footer.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import darkIcon from "@/../public/dark-icon.svg";
+import darkLogo from "@/../public/dark-logo.svg";
 
 export default async function Footer() {
   return (
     <footer className="h-12 px-4 flex items-center justify-center gap-2">
       <div className="text-xs text-center text-gray-500">ShiftAPPens25</div>
-      <Image src={darkIcon} alt="Cherry Pick Logo" width={20} height={20} />
+      <Image src={darkLogo} alt="Cherry Pick Logo" width={20} height={20} />
       <div className="text-xs text-center text-gray-500">
         2025 Cherry Pick &copy;
       </div>
--- cherrypick-fe/src/components/header/logo.tsx
+++ cherrypick-fe/src/components/header/logo.tsx
@@ -1,12 +1,12 @@
 import { makasar } from "@/fonts";
-import icon from "@/../public/icon.svg";
+import logo from "@/../public/logo.svg";
 import Image from "next/image";
 import Link from "next/link";
 
 export default async function Logo() {
   return (
     <Link href="/" className={`flex items-center ${makasar.className}`}>
-      <Image src={icon} alt="Cherry Pick Logo" width={48} height={48} />
+      <Image src={logo} alt="Cherry Pick Logo" width={48} height={48} />
       <span className="text-2xl font-bold text-dark-blue">Cherry Pick</span>
     </Link>
   );
