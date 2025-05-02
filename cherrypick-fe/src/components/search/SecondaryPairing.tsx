7ce7d26e5e60b8974279999af0ef354abbf41429
Process-ing <rbruno.p.o@gmail.com>
2025-05-02 21:56:59 +0100

    Fix secondary results

--- cherrypick-fe/src/components/search/SecondaryPairing.tsx
+++ cherrypick-fe/src/components/search/SecondaryPairing.tsx
@@ -3,18 +3,19 @@
 import Image from "next/image";
 import Link from "next/link";
 import bookTemplate from "@/../public/book-template.png";
+import { Pairing } from "@/types";
 
-export default function SecondaryPairing() {
+export default function SecondaryPairing({ pairing }: { pairing: Pairing }) {
   return (
     <article className="h-36 w-36 relative">
       <Link href={"/pairing/1/2"}>
         <Image
-          src={bookTemplate}
+          src={pairing.item1.cover || bookTemplate}
           alt="Item 1 cover"
           className="h-32 w-32 absolute object-contain"
         />
         <Image
-          src={bookTemplate}
+          src={pairing.item2.cover || bookTemplate}
           alt="Item 2 cover"
           className="h-32 w-32 left-4 top-4 absolute object-contain"
         />
