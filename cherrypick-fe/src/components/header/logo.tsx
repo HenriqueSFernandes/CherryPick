cee03338a03341b5b26150347cfd7f1594a4efa2
Clara Sousa <clarasousa2004@hotmail.com>
2025-05-02 14:23:17 +0100

    Logo

--- cherrypick-fe/src/components/header/logo.tsx
+++ cherrypick-fe/src/components/header/logo.tsx
@@ -6,8 +6,7 @@ import Link from "next/link";
 export default async function Logo() {
   return (
     <Link href="/" className={`flex items-center ${makasar.className}`}>
-      <Image src={logo} alt="Cherry Pick Logo" width={48} height={48} />
-      <span className="text-2xl font-bold text-dark-blue">Cherry Pick</span>
+      <Image src={logo} alt="Cherry Pick Logo" />
     </Link>
   );
 }
