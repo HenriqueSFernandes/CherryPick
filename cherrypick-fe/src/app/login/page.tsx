48f1e3a1b5f64bd6d3c4825be68fe79f2c774b7e
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2025-05-02 09:59:25 +0100

    feat: account get

--- cherrypick-fe/src/app/login/page.tsx
+++ cherrypick-fe/src/app/login/page.tsx
@@ -1,60 +1,46 @@
 "use client";
 
-import { login, register } from "@/lib/Authenticator";
+import { login, logout } from "@/lib/Authenticator";
+import { useState } from "react";
 
 export default function LoginPage() {
-	const handleLogin = async (event: React.FormEvent) => {
-		event.preventDefault();
-		const formData = new FormData(event.currentTarget as HTMLFormElement);
-		const username = formData.get("email") as string;
-		const password = formData.get("password") as string;
-		await login(username, password);
-	};
+  const [currentUser, setCurrentUser] = useState("");
 
-	const handleRegister = async (event: React.FormEvent) => {
-		event.preventDefault();
-		const formData = new FormData(event.currentTarget as HTMLFormElement);
-		const username = formData.get("email") as string;
-		const password = formData.get("password") as string;
-		const confirmPassword = formData.get("confirm-password") as string;
-		await register(username, password, confirmPassword);
-	};
+  const handleLogin = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget as HTMLFormElement);
+    const username = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const newUsername = await login(username, password);
+    if (!newUsername) {
+      alert("Login failed");
+      return;
+    }
+    setCurrentUser(newUsername);
+  };
 
-	return (
-		<div>
-			<h1>Login</h1>
-			<form onSubmit={handleLogin}>
-				<div>
-					<label htmlFor="email">Email:</label>
-					<input type="email" id="email" name="email" />
-				</div>
-				<div>
-					<label htmlFor="password">Password:</label>
-					<input type="password" id="password" name="password" />
-				</div>
-				<button type="submit">Login</button>
-			</form>
+  const handleLogout = async () => {
+    await logout();
+    setCurrentUser("");
+  };
 
-			<h1>Register</h1>
-			<form onSubmit={handleRegister}>
-				<div>
-					<label htmlFor="email">Email:</label>
-					<input type="email" id="email" name="email" />
-				</div>
-				<div>
-					<label htmlFor="password">Password:</label>
-					<input type="password" id="password" name="password" />
-				</div>
-				<div>
-					<label htmlFor="confirm-password">Confirm Password:</label>
-					<input
-						type="password"
-						id="confirm-password"
-						name="confirm-password"
-					/>
-				</div>
-				<button type="submit">Register</button>
-			</form>
-		</div>
-	);
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <p>Current User: {currentUser}</p>
+      <button onClick={handleLogout}>Logout</button>
+
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
