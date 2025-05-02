76b08d8b7723b6e0ca674b2e047caf6b3b66f2cf
Process-ing <rbruno.p.o@gmail.com>
2025-05-02 01:45:02 +0100

    Specify font subsets

--- cherrypick-fe/src/fonts.ts
+++ cherrypick-fe/src/fonts.ts
@@ -2,8 +2,11 @@ import { Montserrat, Noto_Serif_Makasar } from "next/font/google";
 
 const makasar = Noto_Serif_Makasar({
   weight: "400",
+  subsets: ["latin"],
 });
 
-const montserrat = Montserrat({});
+const montserrat = Montserrat({
+  subsets: ["latin"],
+});
 
 export { makasar, montserrat };
