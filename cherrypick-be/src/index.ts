1f0ab406b5914989babfb03a2efafe551f480276
Process-ing <rbruno.p.o@gmail.com>
2025-05-02 17:21:59 +0100

    Add item routes

--- cherrypick-be/src/index.ts
+++ cherrypick-be/src/index.ts
@@ -4,12 +4,16 @@ import dotenv from "dotenv";
 dotenv.config({ path: '.env' })
 
 import searchRoutes from "./routes/search";
+import itemRoutes from "./routes/item";
+import pairingRoutes from "./routes/pairing";
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 // Use the search route
 app.use("/search", searchRoutes);
+app.use("/item", itemRoutes);
+app.use("/pairing", pairingRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello from TypeScript + Express!");
