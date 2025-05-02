5b73cb03bede3c2b0a4504c029d036015610086a
Process-ing <rbruno.p.o@gmail.com>
2025-05-02 02:38:38 +0100

    Add gradients

---
+++ cherrypick-fe/src/components/general/gradients.tsx
@@ -0,0 +1,20 @@
+export default async function Gradients() {
+  return (
+    <div className="absolute w-screen h-screen overflow-hidden -z-10">
+      <div
+        className="fixed top-0 -right-[250px] w-[500px] h-[500px] rounded-full opacity-70 blur-xl"
+        style={{
+          background:
+            "radial-gradient(circle, hsla(1, 98%, 24%, 0.4), hsla(1, 98%, 24%, 0.0))",
+        }}
+      ></div>
+      <div
+        className="fixed -bottom-[250px] -left-[250px] w-[500px] h-[500px] rounded-full opacity-70 blur-xl"
+        style={{
+          background:
+            "radial-gradient(circle, hsla(1, 98%, 24%, 0.4), hsla(1, 98%, 24%, 0.0))",
+        }}
+      ></div>
+    </div>
+  );
+}
