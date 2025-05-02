94517fbe43bb80a642fc4cd11231f7e4318d2c86
Process-ing <rbruno.p.o@gmail.com>
2025-05-02 10:07:28 +0100

    Refactor footer

--- cherrypick-fe/src/components/footer/footer.tsx
+++ cherrypick-fe/src/components/footer/footer.tsx
@@ -1,8 +1,13 @@
+import Image from "next/image";
+import darkIcon from "@/../public/dark-icon.svg";
+
 export default async function Footer() {
   return (
-    <footer className="h-12 px-4 flex items-center">
+    <footer className="h-12 px-4 flex items-center justify-center gap-2">
+      <div className="text-xs text-center text-gray-500">ShiftAPPens25</div>
+      <Image src={darkIcon} alt="Cherry Pick Logo" width={20} height={20} />
       <div className="text-xs text-center text-gray-500">
-        &copy; 2025 Cherry Pick. For ShiftAPPens. All rights reserved.
+        2025 Cherry Pick &copy;
       </div>
     </footer>
   );
