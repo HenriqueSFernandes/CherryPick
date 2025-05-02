ecd4deaac385003d00f24cc5e339970d2dd45af7
Process-ing <rbruno.p.o@gmail.com>
2025-05-02 10:07:28 +0100

    Refactor footer

--- cherrypick-fe/src/components/header/logo.tsx
+++ cherrypick-fe/src/components/header/logo.tsx
@@ -1,12 +1,13 @@
 import { makasar } from "@/fonts";
 import icon from "@/../public/icon.svg";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Logo() {
   return (
-    <div className={`flex items-center ${makasar.className}`}>
+    <Link href="/" className={`flex items-center ${makasar.className}`}>
       <Image src={icon} alt="Cherry Pick Logo" width={48} height={48} />
       <span className="text-2xl font-bold text-gray-900">Cherry Pick</span>
-    </div>
+    </Link>
   );
 }
