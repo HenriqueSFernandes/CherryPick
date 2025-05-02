e89ea71f954d2c25a9902c1d1342d928f9f4e38b
Process-ing <rbruno.p.o@gmail.com>
2025-05-02 01:29:27 +0100

    Resize footer letters

--- cherrypick-fe/src/components/footer/footer.tsx
+++ cherrypick-fe/src/components/footer/footer.tsx
@@ -1,7 +1,7 @@
 export default async function Footer() {
   return (
     <footer className="h-12 px-4 flex items-center">
-      <div className="text-center text-gray-500">
+      <div className="text-xs text-center text-gray-500">
         &copy; 2025 Cherry Pick. For ShiftAPPens. All rights reserved.
       </div>
     </footer>
