728816e0ad321487b4326f5802d4a00c389225eb
Process-ing <rbruno.p.o@gmail.com>
2025-05-02 09:19:23 +0100

    Use icon in header logo

--- cherrypick-fe/src/components/header/logo.tsx
+++ cherrypick-fe/src/components/header/logo.tsx
@@ -1,8 +1,11 @@
 import { makasar } from "@/fonts";
+import icon from "@/../public/icon.svg";
+import Image from "next/image";
 
 export default async function Logo() {
   return (
     <div className={`flex items-center ${makasar.className}`}>
+      <Image src={icon} alt="Cherry Pick Logo" width={48} height={48} />
       <span className="text-2xl font-bold text-gray-900">Cherry Pick</span>
     </div>
   );
